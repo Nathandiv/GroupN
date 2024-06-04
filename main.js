771411d7520ad1b3dc555d781b28b7892f03888a
GuguM22 <gugu.mkhwanazi40@gmail.com>
2024-06-04 14:42:06 +0200

    added if and else statements

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ let department;
 let departmentStandard = 500;
 let department7 = 550;
 let percent = 0.2;
+
+
+if (departm === 7) {
+    department = department7;
+} else {
+    department = departmentStandard;
+}
