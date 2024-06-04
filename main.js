79c135afa261f14d01cf87457ab00a3a7f4a6f9a
Nellark <nelakulati@gmail.com>
2024-06-04 12:37:27 +0200

    added

---
+++ main.js
@@ -0,0 +1,13 @@
+let fname = prompt('Enter your name');
+let departm = Number(prompt('Enter Your department')); 
+let salary = Number(prompt('Enter your gross salary'));
+let department = Number(500);
+let department7 = Number(550);
+let percent = Number(0.2);
+
+
+
+if  (Employee = department){
+    console.log(fname +' your net income is: ') +( salary * percent + salary);}
+
+ alert('Hi ' + fname +" your net income is " + (salary * percent + salary ))
